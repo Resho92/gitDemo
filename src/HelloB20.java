6528016292508bb4a194c0b061ceb629876bc3d2
Resho <refikbelcim@gmail.com>
2020-08-01 11:51:48 -0400

    interview

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -5,6 +5,6 @@ public class HelloB20 {
 
         System.out.println("Hello B20, it is my pleasure to be with you!");
 
-
+        System.out.println("Hi my name is Resho thank you for giving me this opportunity");
     }
 }
